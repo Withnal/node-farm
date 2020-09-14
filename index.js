7df5746391a91eb66ea7b3449540661370ebaa7b
Ferdynal <buildwithnal@gmail.com>
2020-09-14 11:40:16 +0700

    req,res url

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const fs = require("fs");
 const http = require("http");
-const { Http2ServerRequest } = require("http2");
+const url = require("url");
 
 // server
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-word",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+
+  console.log(req.url);
   res.end("Hello from the server!");
 });
 
